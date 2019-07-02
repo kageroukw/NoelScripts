ec6fec7e394778ed5f486a5e2dc395d27e49bcb2
矯正人形 <45246543+Kwothsei@users.noreply.github.com>
2019-07-02 20:35:12 +0900

    add launcher file

---
+++ noel_launcher.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+echo ""
+echo ""
+echo "   $(tput setaf 6) ____  _____     ____      _________    _____    "
+echo "   $(tput setaf 6)|_   \|_   _|  .'    '.    _   ___  |  |_   _|   "
+echo "   $(tput setaf 6)  |   \ | |   /  .--.  \    | |_  \_|    | |     "
+echo "   $(tput setaf 6)  | |\ \| |   | |    | |    |  _|  _     | |   _ "
+echo "   $(tput setaf 6) _| |_\   |_  \  '--'  /   _| |___/ |   _| |__/ |"
+echo "   $(tput setaf 6)|_____|\____|  '.____.'   |_________|  |________|"
+echo "			Open-source, multipurpose Discord bot coded in C#. https://noel-bot.tk"
+echo ""
+echo ""
+echo "$(tput setaf 7)Welcome to $(tput setaf 6)Noel $(tput setaf 7)linux launcher."
+echo ""
+choice=4
+	echo "[1] Download"
+	echo "[2] Launch"
+	echo "[3] Exit"
+	echo -n "Choose $(tput setaf 3)[1] $(tput setaf 7)to Download $(tput setaf 6)Noel, $(tput setaf 7)choose $(tput setaf 3)[2] $(tput setaf 7)to Launch $(tput setaf 6)Noel $(tput setaf 7)or $(tput setaf 3)[3] $(tput setaf 7)to $(tput setaf 1)Exit"
+	echo "$(tput setaf 2)"
+while [ "$choice" = "4" ]; do
+read choice
+if [ $choice -eq 1 ]; then \
+
+	echo "$(tput setaf 3)[1] Download"
+	echo ""
+	echo "$(tput setaf 7)Starting download.."
+	sleep 5s
+	git clone https://raw.githubusercontent.com/Kwothsei/Noel.NET
+	echo ""
+	echo "Done!"
+else
+	if [ $choice -eq 2 ]; then \
+		echo "$(tput setaf 3)[2] Launch"
+		echo ""
+		cd Noel.NET.Core || exit
+		echo "$(tput setaf 7)Building.."
+		sleep 5s
+		dotnet build
+		echo "Done"
+		echo "Starting.."
+		sleep 5s
+		dotnet run
+	else
+		if [ $choice -eq 3 ]; then \
+			break
+		else
+			clear
+			echo "[1] Download"
+			echo "[2] Launch"
+			echo "[3] Exit"
+			echo -n "Choose [1] to Download Noel, choose [2] to Launch Noel or [3] to Exit."
+			choice=4
+		fi
+	fi
+fi
+done
+exit 0
