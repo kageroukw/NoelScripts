7cc5912b21b78086f23b448c3cfccb122aa2dfa8
矯正人形 <45246543+Kwothsei@users.noreply.github.com>
2019-07-02 21:14:43 +0900

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,4 +1,6 @@
+apt install git
+apt install curl
+apt install dos2unix
 wget https://raw.githubusercontent.com/Kwothsei/NoelScripts/master/noel_launcher.sh
-apt install
 dos2unix noel_launcher.sh
 sh noel_launcher.sh
